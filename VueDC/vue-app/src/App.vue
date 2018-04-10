139039a7b154af7a0c82e504378aac8eb33b1d58
itesla <itesladevp@gmail.com>
2018-04-09 23:34:23 -0400

    adding new configuration to the app v.0.0.1

--- VueDC/vue-app/src/App.vue
+++ VueDC/vue-app/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <Skills msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <router-link to = "/">Home</router-link>
+      <router-link to = "/about">About</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
@@ -16,12 +20,38 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body{
+  background-color: #EEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
 }
+
+body,html{
+  margin:0;
+  height: 100%;
+}
+
+#app{
+  width: 50%;
+}
+
+nav{
+  padding: 20px 20px 20px 0;
+}
+
+nav a{
+  padding: 10px;
+  text-decoration: none;
+  background: #ffffff;
+  border-radius: 3px;
+  color: rgb(0,, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+
 </style>
---
+++ VueDC/vue-app/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+      <h1>Hello {{ their_name }}, this is an about page</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem quae, delectus provident, error nulla reiciendis voluptatum incidunt neque consequuntur vel soluta quas totam molestias ut. Culpa voluptatem veniam dolor accusantium.</p>
+  </div>
+</template>
+
+<script>
+export default {
+ name:'Skills',
+ data(){
+     return{
+         their_name: this.$route.params.name
+     }
+ }
+}
+</script>
+
